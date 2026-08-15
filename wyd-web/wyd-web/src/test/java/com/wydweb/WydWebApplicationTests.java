821e822bd9dccf875d03fc530476c250e578447d
berti <berti.prog@gmail.com>
2026-08-15 01:18:15 -0300

    feat:: configuração inicial

---
+++ wyd-web/wyd-web/src/test/java/com/wydweb/WydWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wydweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WydWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
